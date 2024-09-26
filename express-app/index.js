930be842faa1603603ae0c4d9689e9d6a7db89a7
max-machin <max.machin@laplateforme.io>
2024-09-26 02:19:31 +0200

    add testing case's

--- express-app/index.js
+++ express-app/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-
 const app = express();
 
 app.get('/', (req, res) => {
@@ -7,5 +6,16 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+// Route pour tester le 404
+app.get('/unknown', (req, res) => {
+  res.status(404).send('Not Found');
+});
+
+// Route pour tester une erreur 500
+app.get('/error', (req, res) => {
+  res.status(500).send('Internal Server Error');
+});
+
 module.exports = app.listen(8080, () => {
+  console.log('Server is running on port 8080');
 });
--- express-app/tests/test-app.js
+++ express-app/tests/test-app.js
@@ -1,24 +1,42 @@
 const chai = require('chai');
-
 const should = chai.should();
 const chaiHttp = require('chai-http');
 
 chai.use(chaiHttp);
 
-const app = require('../index.js');
+const app = require('../index.js'); // Assurez-vous que le chemin est correct
 
 describe('GET /', () => {
-  it('should respond with hello world', (done) => {
-    chai.request(app)
-      .get('/')
-      .end((err, res) => {
-      // there should be no errors
-        should.not.exist(err);
-        // there should be a 200 status code
-        res.status.should.equal(200);
-        // the response should be JSON
-        res.type.should.equal('text/plain');
-        done();
-      });
-  });
+    it('should respond with hello world', (done) => {
+        chai.request(app)
+            .get('/')
+            .end((err, res) => {
+                should.not.exist(err);
+                res.status.should.equal(200);
+                res.type.should.equal('text/plain');
+                done();
+            });
+    });
+
+    it('should respond with 404 for unknown route', (done) => {
+        chai.request(app)
+            .get('/unknown')
+            .end((err, res) => {
+                should.not.exist(err);
+                res.status.should.equal(404);
+                done();
+            });
+    });
+
+
+    it('should return a 500 error for internal server error', (done) => {
+        // Simuler une erreur interne pour un test
+        chai.request(app)
+            .get('/error') // Assurez-vous que vous avez une route qui renvoie une erreur
+            .end((err, res) => {
+                should.not.exist(err);
+                res.status.should.equal(500);
+                done();
+            });
+    });
 });
